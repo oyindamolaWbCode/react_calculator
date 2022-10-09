d9c4e54299442a0fe1f1a18dec90837bf2c78048
Blackbee <70699255+oyindamolaWbCode@users.noreply.github.com>
2022-10-09 12:22:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Calculator from "./Components/calculator";
+
+function App() {
+  return (
+    <div className="content">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
